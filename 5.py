14efee58e3fdf19b772af5b482a547a328863adb
Alexei Baida <alexbayda01@gmail.com>
2020-06-11 22:58:34 +0300

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,1 +1,10 @@
-# I have no fucking idea except brutforce :)
+# Smallest multiple
+import time
+
+start = time.time()
+for i in range(1, 1000000000):
+    if i % 11 == 0 and i % 12 == 0 and i % 13 == 0 and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0 and i % 20 == 0:
+        print(i)
+
+end = time.time()
+print('Time of execution = ' + str(end - start))
