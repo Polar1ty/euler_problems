3881e9e2d8ecfb45fd4c7a3982863aea39ccdf1b
Alexei Baida <alexbayda01@gmail.com>
2020-06-25 08:54:56 +0300

    Add files via upload

--- 25 1000-digit Fibonacci number.py
+++ 25 1000-digit Fibonacci number.py
@@ -1,10 +1,15 @@
 # 1000-digit Fibonacci number
+import time
 
-fibo = [0, 1, 1]  # Zero added to see real index of 1000-digit number according to task
+start = time.time()
+fibo = [1, 1]
 
 while 1:
     new_f = fibo[-2] + fibo[-1]
     fibo.append(new_f)
-    if len(str(new_f)) == 1000:
-        print('Result =', fibo.index(new_f))
+    if len(str(new_f)) == 10000:
+        print('Result =', fibo.index(new_f) + 1)
         break
+
+end = time.time()
+print('Time of execution = ' + str(end - start))
